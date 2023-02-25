b79457b0d5ecddc988131234b7ca6aebbc90b441
Your Name <edward.lenzner@gmail.com>
2023-02-25 20:09:12 +1100

    iojlm

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const container = document.getElementById("page")
-const loadBox = document.createElement('div')
-loadBox.className = "loadBox"
-container.appendChild(loadBox)
+this.onload = () => {
+    const container = document.getElementById("page")
+    const loadBox = document.createElement('div')
+    loadBox.className = "loadBox"
+    container.appendChild(loadBox)
+}
 
-loadBox.animate({
-    width: `100%`,
-    height: `100%`
-}, { duration: 1200, fill: "forwards" });
+function start() {
+    loadBox.animate({
+        width: `100%`,
+        height: `100%`
+    }, { duration: 1200, fill: "forwards" });
+}
