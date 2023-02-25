298f509562aa7cb5ffc0aeec66d7b0b1e99c22f6
Your Name <edward.lenzner@gmail.com>
2023-02-25 19:56:29 +1100

    Setup?

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.getElementById("page")
+const loadBox = document.createElement('div')
+loadBox.className = "loadBox"
+container.appendChild(loadBox)
+
+loadBox.animate({
+    width: `100%`,
+    height: `100%`
+}, { duration: 1200, fill: "forwards" });
